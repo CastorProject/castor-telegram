9c13674b9bd0900a14842454c2b82ee307151d36
root <root@raspberrypi>
2019-11-27 08:31:12 -0500

    week update

--- scripts/commands/movements/keyboards.py
+++ scripts/commands/movements/keyboards.py
@@ -5,15 +5,17 @@ from utils.create_keyboard import createKeyboard
 
 def movements_keyboard():
     keyboardStr = [
-        ['Saludo', 'Me alegro', 'Como estás'],
-        ['Muy bien', 'Jugar', 'Adivina'],
-        ['Sigue intentando', 'Felicitar', 'Despedida', 'Brazo'],
+        ['Saludo', 'Me alegro', 'Como estás', 'Muy bien'],
+        ['¿Color?', '¿Animal?', 'También'],
+        ['¿Jugar?', 'Adivina', 'Sigue intentando', 'Felicitar'],
+        ['Despedida', 'ChaoO', 'ChaoA'],
         ['◀️ Atrás']
     ]
     callbackStr = [
-        ['greet', 'nicetomeet', 'howru'],
-        ['fine', 'play', 'guess'],
-        ['tryagain', 'nice', 'bye', 'wave'],
+        ['greet', 'nicetomeet', 'howru', 'fine'],
+        ['color', 'animal', 'metoo'],
+        ['wantplay', 'guess',  'tryagain', 'nice'],
+        ['byeL', 'byeB', 'byeG'],
         ['back']
     ]
     return createKeyboard(keyboardStr, "inline", callbackStr)
